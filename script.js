146d787263b10ca866118c9e1eb83974783c5291
sophiamrobles <srobles2@hawk.iit.edu>
2024-04-03 20:49:01 -0500

    set up structure to javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-javascript page 
+const billTotal = document.getElementById('billTotal');
+const tipSlider = document.getElementById('tipSlider');
+const tipPercentage = document.getElementById('tipPercentage');
+const tipAmount = document.getElementById('tipAmount');
+const totalBillWTip = document.getElementById('totalBillWTip');
+
+document.getElementById('calcTip').addEventListener('input', function(){
+    
+}
