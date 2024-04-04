6a208496dfa91c11efb2ea231763136a1292059c
sophiamrobles <srobles2@hawk.iit.edu>
2024-04-04 10:41:42 -0500

    correct brackets

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ const tipAmount = document.getElementById('tipAmount');
 const totalBillWTip = document.getElementById('totalBillWTip');
 
 document.getElementById('calcTip').addEventListener('input', function(){
-    
-}
+    const billTotal = parseFloat(billTotal.value);
+    const tipPercentage = parseFloat(tipSlider.value);
+
+    if (isNaN(billTotal)){
+        alert('Please enter a numerical value for bill total.');
+        return;
+    }
+
+    const tipAmount = (billTotal*tipPercentage/100);
+    const totalBillWTip = billTotal + tipAmount;
+    tipPercentage.value = tipPercentage + "%";
+    tipAmount.value = tipAmount.toFixed(2);
+    totalBillWTip = totalBillWTip.toFixed(2);
+});
